97c61282790e9951d1d7d34d4af24737389e9ccd
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-04-16 20:14:38 +0530

    handle async runtime using clearInterval

---
+++ runtimejs/asyncbehavior.js
@@ -0,0 +1,18 @@
+
+
+let timer=setInterval(function exec(){
+    console.log("hello world");
+},1000)
+function timeUsingLoop() {
+    console.log("loop start");
+    for (let i = 0; i < 1000000000;i++) {
+      // console.log();
+    }
+    setTimeout(()=>{
+        console.log("terminate process start");
+        clearInterval(timer)
+    },10000)
+    // clearInterval(timer);
+    console.log("loop end");
+}
+timeUsingLoop();
