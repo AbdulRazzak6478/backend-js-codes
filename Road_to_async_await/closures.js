c9c1ba829c31d320afd6dfeffb048376ebeaad4c
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-04-29 23:24:43 +0530

    iterator is done

--- Road_to_async_await/closures.js
+++ Road_to_async_await/closures.js
@@ -1,25 +1,28 @@
 
-function fetchNextElement(){
+function fetchNextElement(arr){
     let i=0;
     let j=10;
-    function innerElement(){
+    function next(){
+        if(i=== arr.length)return {value:undefined,done:true};
         i+=1;
         j=j+10;
-        return i;
+        return {value:i,done:false};
     }
-    return innerElement;
+    return {next};
 }
 
-const ele=fetchNextElement();
+const ele=fetchNextElement([1,2,3,4,5,6,7]);
 console.log("without calling ",ele);
-console.log("with calling 1 :",ele());
-console.log("with calling 2 :",ele());
-console.log("with calling 3 :",ele());
-console.log("with calling 4 :",ele());
-console.log("with calling 5 :",ele());
-console.log("with calling 6 :",ele());
+console.log("with calling 1 :",ele.next());
+console.log("with calling 2 :",ele.next());
+console.log("with calling 3 :",ele.next());
+console.log("with calling 4 :",ele.next());
+console.log("with calling 5 :",ele.next());
+console.log("with calling 6 :",ele.next());
+console.log("with calling 7 :",ele.next());
+console.log("with calling 8 :",ele.next());
 
-const arr={func:ele};
+const arr={func:ele.next};
 console.log(arr);
 console.log(arr.func());
 console.log(arr.func());
---
+++ Road_to_async_await/iterator.js
@@ -0,0 +1,11 @@
+const arr=[1,2,3,4,5,6,7];
+const arr2=arr[Symbol.iterator]();
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
+console.log(arr2.next());
